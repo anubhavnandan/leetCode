1fc91510e1e36c165e738f0b13a5f3f714644341
Anubhav <ratioformation@gmail.com>
2021-09-08 11:51:44 +0530

    Day 1 Lost

---
+++ Day 1.cpp
@@ -0,0 +1,20 @@
+//C++ 11
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	   void Method(){
+	   	cout<<"Hello World";
+	   }
+};
+int main(){
+
+Solution Obj;
+
+Obj.Method();
+
+return 0;
+}
+
