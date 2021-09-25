8dc36d2753711f9c32793952317300b2805cbe60
Anubhav <ratioformation@gmail.com>
2021-09-26 05:08:30 +0530

    Create 35_Search Insert Position.cpp
    Day 4

---
+++ 35_Search Insert Position.cpp
@@ -0,0 +1,48 @@
+//C++ 11
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+ 	int searchInsert(vector<int>& nums, int target) {
+        int S = nums.size()-1;
+        int mid,begin=0,end=S;  
+        if(target<=nums[0])
+                return 0;
+        else if(target==nums[S])
+            return S;
+        else if(target>nums[S])
+                return S+1;
+        else{
+        	mid=(begin+end)/2;
+            while(mid>1 || mid<S){
+                mid=(begin+end)/2;
+                if(nums[mid]==target)
+                    return mid;
+                else if(target < nums[mid] && end != mid)
+                    end=mid;
+                else if(target > nums[mid] && begin !=mid)
+                    begin=mid;
+                else 
+                    break;
+            }
+        }   
+        return target>nums[mid]?mid+1:mid;
+    }
+};
+
+int main(){
+
+int a[] = {1,2,4,6,7};
+int t = 3;
+vector <int> V(begin(a),end(a));
+
+Solution Obj;
+cout<<Obj.searchInsert(V,t);
+
+return 0;
+}
+
