2cef3b64490c949d96caba01377075d32df7c954
Anubhav <ratioformation@gmail.com>
2021-10-07 16:22:46 +0530

    Day 8

--- 3_Longest Substring Without Repeating Characters.cpp
+++ 3_Longest Substring Without Repeating Characters.cpp
@@ -1,25 +1,25 @@
 //C++ 11
-#include<iostream>
-#include<string>
-#include<unordered_map>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> uniqueSubString;
-        int maxWindowSize=0,currentWindowSize=0;
+        unordered_map<char,int> dictionary;
+        int maxWindowSize=0,currentWindowSize=0,windowStart=0,windowEnd=0;
         for(int i=0; i<s.size(); i++){
-        	if(uniqueSubString.count(s[i])>0){
-        		++currentWindowSize;
-        		currentWindowSize=currentWindowSize - uniqueSubString[s[i]];
-        		uniqueSubString[s[i]]=currentWindowSize;
+        	if(dictionary.count(s[i])>0){
+        		if(dictionary[s[i]]>=windowStart)
+        		windowStart=dictionary[s[i]]+1;
+        		dictionary[s[i]]=i;
 			}else{
-				++currentWindowSize;
-				uniqueSubString.insert({s[i],currentWindowSize});
+				dictionary.insert({s[i],i});
 			}
-			maxWindowSize=maxWindowSize>currentWindowSize ?  maxWindowSize : currentWindowSize;
+			windowEnd=i;
+			currentWindowSize=windowEnd-windowStart+1;
+//			cout<<s[i]<<" "<<windowEnd<<" "<<windowStart<<" "<<currentWindowSize<<" "<<endl;
+			maxWindowSize=currentWindowSize>maxWindowSize ?   currentWindowSize : maxWindowSize;
 		}
 		return maxWindowSize;
     }
@@ -28,7 +28,7 @@ int main(){
 
 Solution Obj;
 
-cout<<Obj.lengthOfLongestSubstring("tmmzuxt");
+cout<<Obj.lengthOfLongestSubstring("aabaab!bb");
 //abcabcbb
 //bbbbb
 return 0;
